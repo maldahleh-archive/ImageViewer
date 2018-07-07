c91c58dbc2f9544adc73b23176decc25b6114b2b
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-07-06 23:54:45 -0400

    Include provided starter files

---
+++ ImageViewer/Starter Files/PhotoListController.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoListController.swift
+//  ImageViewer
+//
+//  Created by Pasan Premaratne on 9/26/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+final class PhotoListController: UIViewController {
+    
+    @IBOutlet weak var photosCollectionView: UICollectionView!
+    
+    let context = CoreDataStack().managedObjectContext
+    
+    lazy var dataSource: PhotosDataSource = {
+        let request: NSFetchRequest<Photo> = Photo.fetchRequest()
+        return PhotosDataSource(fetchRequest: request, managedObjectContext: self.context, collectionView: self.photosCollectionView)
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        photosCollectionView.dataSource = dataSource
+    }
+}
