f8baafaf5371e3cabcbc02be534d2b8b86630e38
Mohammed Al-Dahleh <maldahleh@gmail.com>
2018-07-07 23:45:46 -0400

    Add necessary detail controllers

---
+++ ImageViewer/Photos/PhotoView/PhotoPageController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoPageController.swift
+//  ImageViewer
+//
+//  Created by Mohammed Al-Dahleh on 2018-07-07.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class PhotoPageController: UIPageViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ImageViewer/Photos/PhotoView/PhotoViewerController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoViewerController.swift
+//  ImageViewer
+//
+//  Created by Mohammed Al-Dahleh on 2018-07-07.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewerController: UIViewController {
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
